fe9c2e39070c2fe4224edc4dac6a6ac42fc15466
Ingenieria Sistemas11 <isi11@lab.uca.edu.ni>
2023-05-24 16:37:44 -0600

    Se rediseña el formulario principal

---
+++ RegistrosHR/RegistrosHR/FrmPrincipal.Designer.vb
@@ -0,0 +1,134 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FrmPrincipal
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GbxDatosBusqueda = New System.Windows.Forms.GroupBox()
+        Me.BtnBuscar = New System.Windows.Forms.Button()
+        Me.TxtBuscar = New System.Windows.Forms.TextBox()
+        Me.CbxCampo = New System.Windows.Forms.ComboBox()
+        Me.LblFiltrar = New System.Windows.Forms.Label()
+        Me.GbxRegistros = New System.Windows.Forms.GroupBox()
+        Me.DgvRegistrosEmpleado = New System.Windows.Forms.DataGridView()
+        Me.GbxDatosBusqueda.SuspendLayout()
+        Me.GbxRegistros.SuspendLayout()
+        CType(Me.DgvRegistrosEmpleado, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'GbxDatosBusqueda
+        '
+        Me.GbxDatosBusqueda.Controls.Add(Me.BtnBuscar)
+        Me.GbxDatosBusqueda.Controls.Add(Me.TxtBuscar)
+        Me.GbxDatosBusqueda.Controls.Add(Me.CbxCampo)
+        Me.GbxDatosBusqueda.Controls.Add(Me.LblFiltrar)
+        Me.GbxDatosBusqueda.Location = New System.Drawing.Point(14, 12)
+        Me.GbxDatosBusqueda.Name = "GbxDatosBusqueda"
+        Me.GbxDatosBusqueda.Size = New System.Drawing.Size(730, 75)
+        Me.GbxDatosBusqueda.TabIndex = 0
+        Me.GbxDatosBusqueda.TabStop = False
+        Me.GbxDatosBusqueda.Text = "Seleccione por que campo desea buscar "
+        '
+        'BtnBuscar
+        '
+        Me.BtnBuscar.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnBuscar.Location = New System.Drawing.Point(645, 33)
+        Me.BtnBuscar.Name = "BtnBuscar"
+        Me.BtnBuscar.Size = New System.Drawing.Size(75, 23)
+        Me.BtnBuscar.TabIndex = 3
+        Me.BtnBuscar.Text = "Buscar"
+        Me.BtnBuscar.UseVisualStyleBackColor = True
+        '
+        'TxtBuscar
+        '
+        Me.TxtBuscar.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TxtBuscar.Location = New System.Drawing.Point(266, 33)
+        Me.TxtBuscar.Name = "TxtBuscar"
+        Me.TxtBuscar.Size = New System.Drawing.Size(368, 22)
+        Me.TxtBuscar.TabIndex = 2
+        '
+        'CbxCampo
+        '
+        Me.CbxCampo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.CbxCampo.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CbxCampo.FormattingEnabled = True
+        Me.CbxCampo.Items.AddRange(New Object() {"Profesion", "Nombres", "Apellidos"})
+        Me.CbxCampo.Location = New System.Drawing.Point(89, 31)
+        Me.CbxCampo.Name = "CbxCampo"
+        Me.CbxCampo.Size = New System.Drawing.Size(150, 24)
+        Me.CbxCampo.TabIndex = 1
+        '
+        'LblFiltrar
+        '
+        Me.LblFiltrar.AutoSize = True
+        Me.LblFiltrar.Location = New System.Drawing.Point(18, 36)
+        Me.LblFiltrar.Name = "LblFiltrar"
+        Me.LblFiltrar.Size = New System.Drawing.Size(65, 13)
+        Me.LblFiltrar.TabIndex = 0
+        Me.LblFiltrar.Text = "Filtrar por:"
+        '
+        'GbxRegistros
+        '
+        Me.GbxRegistros.Controls.Add(Me.DgvRegistrosEmpleado)
+        Me.GbxRegistros.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GbxRegistros.Location = New System.Drawing.Point(14, 106)
+        Me.GbxRegistros.Name = "GbxRegistros"
+        Me.GbxRegistros.Size = New System.Drawing.Size(739, 332)
+        Me.GbxRegistros.TabIndex = 1
+        Me.GbxRegistros.TabStop = False
+        Me.GbxRegistros.Text = "Registros Encontrados: 0"
+        '
+        'DgvRegistrosEmpleado
+        '
+        Me.DgvRegistrosEmpleado.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DgvRegistrosEmpleado.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.DgvRegistrosEmpleado.Location = New System.Drawing.Point(3, 18)
+        Me.DgvRegistrosEmpleado.Name = "DgvRegistrosEmpleado"
+        Me.DgvRegistrosEmpleado.Size = New System.Drawing.Size(733, 311)
+        Me.DgvRegistrosEmpleado.TabIndex = 0
+        '
+        'FrmPrincipal
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(765, 450)
+        Me.Controls.Add(Me.GbxRegistros)
+        Me.Controls.Add(Me.GbxDatosBusqueda)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Name = "FrmPrincipal"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
+        Me.Text = "Reporte de Empleado por Profesion"
+        Me.GbxDatosBusqueda.ResumeLayout(False)
+        Me.GbxDatosBusqueda.PerformLayout()
+        Me.GbxRegistros.ResumeLayout(False)
+        CType(Me.DgvRegistrosEmpleado, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GbxDatosBusqueda As GroupBox
+    Friend WithEvents LblFiltrar As Label
+    Friend WithEvents CbxCampo As ComboBox
+    Friend WithEvents BtnBuscar As Button
+    Friend WithEvents TxtBuscar As TextBox
+    Friend WithEvents GbxRegistros As GroupBox
+    Friend WithEvents DgvRegistrosEmpleado As DataGridView
+End Class
---
+++ RegistrosHR/RegistrosHR/FrmPrincipal.vb
@@ -0,0 +1,31 @@
+﻿Imports Microsoft.Reporting.WinForms
+Public Class FrmPrincipal
+
+    Dim tblEmp As New DsDBHR.RptJobEmployeesDataTable
+    Dim rptEmpleados As New DsDBHRTableAdapters.RptJobEmployeesTableAdapter
+    Dim tbl As New DataTable
+
+    Private Sub FrmPrincipal_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        llenarRegistros()
+    End Sub
+
+    Sub llenarRegistros()
+        rptEmpleados.Fill(tblEmp)
+        DgvRegistrosEmpleado.DataSource = tblEmp
+        DgvRegistrosEmpleado.Refresh()
+        GbxRegistros.Text = "Registros guardados: " & DgvRegistrosEmpleado.Rows.Count
+    End Sub
+
+    Private Function crearTabla(query) As DataTable
+        Dim tbl As DataTable = New DataTable("tblEmp")
+
+        tbl.Columns.Add("job_title")
+        tbl.Columns.Add("first_name")
+        tbl.Columns.Add("last_name")
+        tbl.Columns.Add("email")
+        tbl.Columns.Add("phone_number")
+        tbl.Columns.Add("hire_date")
+        tbl.Columns.Add("salary")
+        Return tbl
+    End Function
+End Class
